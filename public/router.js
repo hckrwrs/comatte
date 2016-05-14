aabcd93bba5ca71d303faa2dac180983475ded57
You Kobayashi <sweep.3092@gmail.com>
2016-05-14 18:45:43 +0900

    routerを乗せる仕組みが出来た気がする

--- public/router.js
+++ public/router.js
@@ -4,21 +4,31 @@ var router = new VueRouter()
 
 router.map({
   '/sign_in': {
-    component: Vue.extend({})
+    component: Vue.extend({
+      el: '#sign_in'
+    })
   },
   '/sign_up': {
-    component: Vue.extend({})
+    component: Vue.extend({
+      el: '#sign_up'
+    })
   },
   '/guchis': {
-    component: Vue.extend({}),
+    component: Vue.extend({
+      el: '#guchis'
+    }),
     auth: true
   },
   '/guchis/:guchi_id': {
-    component: Vue.extend({}),
+    component: Vue.extend({
+      el: '#guchi'
+    }),
     auth: true
   },
   '/guchis/:guchi_id/replies': {
-    component: Vue.extend({}),
+    component: Vue.extend({
+      el: '#replies'
+    }),
     auth: true
   },
 });
